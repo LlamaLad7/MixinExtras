12cdb699249b73f523b39f19cc95b0afa89a6818
LlamaLad7 <l3gomindstorms@gmail.com>
2022-01-15 17:16:34 +0000

    Bump to 0.0.2 so JitPack hopefully doesn't screw anything up again.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "com.llamalad7"
-version = "0.0.1"
+version = "0.0.2"
 
 repositories {
     mavenCentral()
--- src/main/java/com/llamalad7/mixinextras/MixinExtrasBootstrap.java
+++ src/main/java/com/llamalad7/mixinextras/MixinExtrasBootstrap.java
@@ -7,6 +7,8 @@ import org.spongepowered.asm.mixin.injection.struct.InjectionInfo;
 public class MixinExtrasBootstrap {
     private static boolean initialized = false;
 
+    public static final String VERSION = "0.0.2";
+
     public static void init() {
         if (!initialized) {
             initialized = true;
