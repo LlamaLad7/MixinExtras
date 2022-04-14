3773ceb178969692e52eac2fecc00751b8f8df23
LlamaLad7 <l3gomindstorms@gmail.com>
2022-04-14 17:54:15 +0100

    Version 0.0.8.

--- src/main/java/com/llamalad7/mixinextras/MixinExtrasBootstrap.java
+++ src/main/java/com/llamalad7/mixinextras/MixinExtrasBootstrap.java
@@ -14,7 +14,7 @@ public class MixinExtrasBootstrap {
      */
     @Deprecated
     @SuppressWarnings("DeprecatedIsStillUsed")
-    public static final String VERSION = "0.0.7";
+    public static final String VERSION = "0.0.8";
 
     public static void init() {
         if (!initialized) {
