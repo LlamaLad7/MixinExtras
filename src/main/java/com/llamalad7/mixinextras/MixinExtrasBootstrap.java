98551d57a2022a638a497a37ce15c38a1efa224b
LlamaLad7 <l3gomindstorms@gmail.com>
2022-04-14 17:48:35 +0100

    Improvement: Add `MixinExtrasBootstrap#getVersion` as a replacement for the `VERSION` field. Derp, constant inlining made the field kind of useless...

--- src/main/java/com/llamalad7/mixinextras/MixinExtrasBootstrap.java
+++ src/main/java/com/llamalad7/mixinextras/MixinExtrasBootstrap.java
@@ -8,6 +8,12 @@ import org.spongepowered.asm.mixin.injection.struct.InjectionInfo;
 public class MixinExtrasBootstrap {
     private static boolean initialized = false;
 
+    /**
+     * @deprecated As of 0.0.8, as the field becomes kind of pointless when it gets inlined at compile-time.
+     * Use {@link #getVersion()} instead.
+     */
+    @Deprecated
+    @SuppressWarnings("DeprecatedIsStillUsed")
     public static final String VERSION = "0.0.7";
 
     public static void init() {
@@ -18,5 +24,9 @@ public class MixinExtrasBootstrap {
             InjectionInfo.register(ModifyReceiverInjectionInfo.class);
             InjectionInfo.register(WrapWithConditionInjectionInfo.class);
         }
+    }
+
+    public static String getVersion() {
+        return VERSION;
     }
 }
