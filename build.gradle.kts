905217d619abe07f35470dbd8d23f27a20588ece
LlamaLad7 <l3gomindstorms@gmail.com>
2022-08-26 03:32:02 +0100

    Version 0.1.0-rc2

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "com.llamalad7"
-version = "0.1.0-rc1"
+version = "0.1.0-rc2"
 
 repositories {
     mavenCentral()
--- src/main/java/com/llamalad7/mixinextras/MixinExtrasBootstrap.java
+++ src/main/java/com/llamalad7/mixinextras/MixinExtrasBootstrap.java
@@ -13,7 +13,7 @@ public class MixinExtrasBootstrap {
      */
     @Deprecated
     @SuppressWarnings("DeprecatedIsStillUsed")
-    public static final String VERSION = "0.1.0-rc1";
+    public static final String VERSION = "0.1.0-rc2";
 
     public static void init() {
         if (!initialized) {
