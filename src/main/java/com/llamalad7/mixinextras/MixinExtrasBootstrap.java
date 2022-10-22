9f2c36e83f16d99b205aa3651f97eaca00f177fa
LlamaLad7 <l3gomindstorms@gmail.com>
2022-10-23 00:59:13 +0100

    Change: Make `MixinExtrasBootstrap#VERSION` private. Not sure why I didn't do this originally, it would have been inlined if anyone ever used it.

--- src/main/java/com/llamalad7/mixinextras/MixinExtrasBootstrap.java
+++ src/main/java/com/llamalad7/mixinextras/MixinExtrasBootstrap.java
@@ -6,14 +6,7 @@ import org.spongepowered.asm.mixin.injection.struct.InjectionInfo;
 
 public class MixinExtrasBootstrap {
     private static boolean initialized = false;
-
-    /**
-     * @deprecated As of 0.0.8, as the field becomes kind of pointless when it gets inlined at compile-time.
-     * Use {@link #getVersion()} instead.
-     */
-    @Deprecated
-    @SuppressWarnings("DeprecatedIsStillUsed")
-    public static final String VERSION = "0.1.0-rc5";
+    private static final String VERSION = "0.1.0-rc5";
 
     public static void init() {
         if (!initialized) {
