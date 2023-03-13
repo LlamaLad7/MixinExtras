7057b1d740dd39578210c0d73e4bb08351b2d833
LlamaLad7 <l3gomindstorms@gmail.com>
2023-03-13 23:12:48 +0000

    New: Forge-specific artifact. This is a wrapper which includes the original dependency in the same way as JarJar so that compatibility is maintained with anyone JarJar'ing the common artifact.

--- fabric/build.gradle.kts
+++ fabric/build.gradle.kts
@@ -1,12 +1,3 @@
-repositories {
-    maven("https://maven.fabricmc.net/")
-}
-
-dependencies {
-    implementation(rootProject)
-    compileOnly("net.fabricmc:fabric-loader:0.14.11")
-}
-
 tasks.withType<ProcessResources> {
     inputs.property("version", version)
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,3 @@
 rootProject.name = "mixinextras"
 include("fabric")
+include("forge")
