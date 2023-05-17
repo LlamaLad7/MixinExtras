0bc24865517868317319d2f9a8465a53a897b319
LlamaLad7 <l3gomindstorms@gmail.com>
2023-05-17 20:15:08 +0100

    Version 0.2.0-beta.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ allprojects {
     apply(plugin = "maven-publish")
 
     group = "com.llamalad7"
-    version = "0.2.0-beta.7"
+    version = "0.2.0-beta.8"
 
     repositories {
         mavenCentral()
