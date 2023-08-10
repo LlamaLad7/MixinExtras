8495d1d1bb008a473fda247e7da007d3ce56b43f
LlamaLad7 <l3gomindstorms@gmail.com>
2023-08-10 02:57:45 +0100

    Fix: Use arrays as the backing values of the `Blackboard`. For some reason the properties are immutable when running under the ModLauncher service.

--- src/main/java/com/llamalad7/mixinextras/utils/Blackboard.java
+++ src/main/java/com/llamalad7/mixinextras/utils/Blackboard.java
@@ -5,15 +5,19 @@ import org.spongepowered.asm.service.MixinService;
 
 /**
  * Helpers for the Mixin property service.
+ * We have to use arrays as the backing values because on ModLauncher the properties are immutable, for some reason.
  */
+@SuppressWarnings("unchecked")
 public class Blackboard {
     private static final IGlobalPropertyService SERVICE = MixinService.getGlobalPropertyService();
 
     public static <T> T get(String key) {
-        return SERVICE.getProperty(SERVICE.resolveKey(key));
+        Object[] impl = SERVICE.getProperty(SERVICE.resolveKey(key));
+        return impl == null ? null : (T) impl[0];
     }
 
     public static void put(String key, Object value) {
-        SERVICE.setProperty(SERVICE.resolveKey(key), value);
+        SERVICE.setProperty(SERVICE.resolveKey(key), new Object[1]);
+        SERVICE.<Object[]>getProperty(SERVICE.resolveKey(key))[0] = value;
     }
 }
