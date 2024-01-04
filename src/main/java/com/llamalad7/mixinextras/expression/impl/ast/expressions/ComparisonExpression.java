1026894b659f8a1730513dc56f6cf371e7a01745
LlamaLad7 <l3gomindstorms@gmail.com>
2024-01-04 01:21:09 +0000

    Fix: Resolve NPE in ComparisonExpression.Operator#isComplexComparison.

--- src/main/java/com/llamalad7/mixinextras/expression/impl/ast/expressions/ComparisonExpression.java
+++ src/main/java/com/llamalad7/mixinextras/expression/impl/ast/expressions/ComparisonExpression.java
@@ -141,6 +141,9 @@ public class ComparisonExpression implements Expression {
         }
 
         private boolean isComplexComparison(AbstractInsnNode insn) {
+            if (insn == null) {
+                return false;
+            }
             switch (insn.getOpcode()) {
                 case LCMP:
                 case FCMPL:
