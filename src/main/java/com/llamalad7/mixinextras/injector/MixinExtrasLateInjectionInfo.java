133fb864a1e7a61230c7cdeae9fb433ccdc1d11d
LlamaLad7 <l3gomindstorms@gmail.com>
2024-01-15 17:55:45 +0000

    Fix: Add callback notifications early for late-applying injectors so they pass group validation.

--- src/main/java/com/llamalad7/mixinextras/injector/MixinExtrasLateInjectionInfo.java
+++ src/main/java/com/llamalad7/mixinextras/injector/MixinExtrasLateInjectionInfo.java
@@ -2,10 +2,14 @@ package com.llamalad7.mixinextras.injector;
 
 import org.objectweb.asm.tree.AnnotationNode;
 import org.objectweb.asm.tree.MethodNode;
+import org.spongepowered.asm.mixin.injection.struct.InjectionNodes.InjectionNode;
 import org.spongepowered.asm.mixin.transformer.MixinTargetContext;
+
+import java.util.List;
 
 public abstract class MixinExtrasLateInjectionInfo extends MixinExtrasInjectionInfo implements LateApplyingInjectorInfo {
     private LateApplyingInjectorInfo injectionInfoToQueue = this;
+    private boolean hasInjectStarted = false;
 
     public MixinExtrasLateInjectionInfo(MixinTargetContext mixin, MethodNode method, AnnotationNode annotation) {
         super(mixin, method, annotation);
@@ -17,11 +21,30 @@ public abstract class MixinExtrasLateInjectionInfo extends MixinExtrasInjectionI
 
     @Override
     public void inject() {
+        hasInjectStarted = true;
+        int callbackTotal = 0;
+        for (List<InjectionNode> nodes : targetNodes.values()) {
+            callbackTotal += nodes.size();
+        }
+        for (int i = 0; i < callbackTotal; i++) {
+            // Have to add these early since we inject after group validation.
+            // May as well add them now, the only way they can fail later is by throwing an exception at which point
+            // it's game over anyway.
+            super.addCallbackInvocation(method);
+        }
         LateInjectionApplicatorExtension.offerInjection(this.mixin.getTarget(), injectionInfoToQueue);
     }
 
     @Override
     public void postInject() {
+    }
+
+    @Override
+    public void addCallbackInvocation(MethodNode handler) {
+        if (!hasInjectStarted) {
+            // We want to allow any additional ones that get added during `prepare` by sugar.
+            super.addCallbackInvocation(handler);
+        }
     }
 
     @Override
