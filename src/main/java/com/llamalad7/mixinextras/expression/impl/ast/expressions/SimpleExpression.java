55b57996f6b038f5d9bf6eb4ed476123b418facd
LlamaLad7 <l3gomindstorms@gmail.com>
2024-04-05 16:21:23 +0100

    Fix: Resolve not being able to modify null values.

--- src/main/java/com/llamalad7/mixinextras/expression/impl/ast/expressions/SimpleExpression.java
+++ src/main/java/com/llamalad7/mixinextras/expression/impl/ast/expressions/SimpleExpression.java
@@ -3,6 +3,7 @@ package com.llamalad7.mixinextras.expression.impl.ast.expressions;
 import com.llamalad7.mixinextras.expression.impl.ExpressionSource;
 import com.llamalad7.mixinextras.expression.impl.flow.FlowValue;
 import com.llamalad7.mixinextras.utils.Decorations;
+import com.llamalad7.mixinextras.utils.TypeUtils;
 import org.objectweb.asm.Type;
 
 public abstract class SimpleExpression extends Expression {
@@ -12,8 +13,12 @@ public abstract class SimpleExpression extends Expression {
 
     @Override
     public void capture(FlowValue node, OutputSink sink) {
-        if (node.getType() != Type.VOID_TYPE) {
-            sink.decorate(node.getInsn(), Decorations.SIMPLE_EXPRESSION_TYPE, node.getType());
+        Type type = node.getType();
+        if (type.equals(TypeUtils.BOTTOM_TYPE)) {
+            type = TypeUtils.OBJECT_TYPE;
+        }
+        if (!type.equals(Type.VOID_TYPE)) {
+            sink.decorate(node.getInsn(), Decorations.SIMPLE_EXPRESSION_TYPE, type);
         }
         super.capture(node, sink);
     }
