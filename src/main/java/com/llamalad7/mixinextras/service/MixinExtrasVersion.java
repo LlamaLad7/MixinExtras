daae24a0acb90ce6e4348a9d71558b1aa872edd6
LlamaLad7 <l3gomindstorms@gmail.com>
2024-05-16 00:50:56 +0100

    Fix: Resolve misnamed version enum member. Derp.

--- src/main/java/com/llamalad7/mixinextras/service/MixinExtrasVersion.java
+++ src/main/java/com/llamalad7/mixinextras/service/MixinExtrasVersion.java
@@ -11,7 +11,7 @@ public enum MixinExtrasVersion {
     V0_2_0_BETA_8("0.2.0-beta.8", -2),
     V0_2_0_BETA_9("0.2.0-beta.9", -1),
     V0_3_4("0.3.4", 213),
-    V0_3_5("0.3.6", 215);
+    V0_3_6("0.3.6", 215);
 
     public static final MixinExtrasVersion LATEST = values()[values().length - 1];
 
