eac09e611689c02ff2eff1421e12dca0305089d7
LlamaLad7 <l3gomindstorms@gmail.com>
2025-01-17 15:34:46 +0000

    Fix: Proper error when incorrect argument count with null arguments.
    Also extract the expensive code to help the JIT.
    Closes #97

--- src/main/java/com/llamalad7/mixinextras/injector/wrapoperation/WrapOperationRuntime.java
+++ src/main/java/com/llamalad7/mixinextras/injector/wrapoperation/WrapOperationRuntime.java
@@ -7,12 +7,22 @@ import java.util.stream.Collectors;
 public class WrapOperationRuntime {
     public static void checkArgumentCount(Object[] args, int expectedArgumentCount, String expectedTypes) {
         if (args.length != expectedArgumentCount) {
-            throw new IncorrectArgumentCountException(String.format(
-                    "Incorrect number of arguments passed to Operation::call! Expected %s but got %s. " +
-                            "Expected types were %s, actual types were %s.",
-                    expectedArgumentCount, args.length,
-                    expectedTypes, Arrays.stream(args).map(it -> it.getClass().getName()).collect(Collectors.joining(", ", "[", "]"))
-            ));
+            // Keep the bad path in a separate method to help the JIT inline this
+            throwIncorrectArgumentCount(args, expectedArgumentCount, expectedTypes);
         }
+    }
+
+    private static void throwIncorrectArgumentCount(Object[] args, int expectedArgumentCount, String expectedTypes) {
+        String actualTypes = Arrays.stream(args)
+                .map(it -> it == null ? "null" : it.getClass().getName())
+                .collect(Collectors.joining(", ", "[", "]"));
+        throw new IncorrectArgumentCountException(
+                String.format(
+                        "Incorrect number of arguments passed to Operation::call! Expected %s but got %s. " +
+                                "Expected types were %s, actual types were %s.",
+                        expectedArgumentCount, args.length,
+                        expectedTypes, actualTypes
+                )
+        );
     }
 }
