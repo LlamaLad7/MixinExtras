78b31b1f58e8dac3c940381d8ac7ffa8bd9b4617
LlamaLad7 <l3gomindstorms@gmail.com>
2026-08-27 18:04:52 +0100

    Fix: Account for `@Redirect#at` possibly being a list.

--- src/main/java/com/llamalad7/mixinextras/wrapper/factory/FactoryRedirectWrapperMixinTransformer.java
+++ src/main/java/com/llamalad7/mixinextras/wrapper/factory/FactoryRedirectWrapperMixinTransformer.java
@@ -9,17 +9,21 @@ import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.util.Annotations;
 
+import java.util.List;
+
 public class FactoryRedirectWrapperMixinTransformer implements MixinTransformer {
     @Override
     public void transform(IMixinInfo mixinInfo, ClassNode mixinNode) {
         for (MethodNode method : mixinNode.methods) {
             AnnotationNode redirect = Annotations.getVisible(method, Redirect.class);
             if (redirect == null) continue;
-            AnnotationNode at = Annotations.getValue(redirect, "at");
-            if (at == null) continue;
-            String value = Annotations.getValue(at);
-            if ("NEW".equals(value)) {
-                wrapInjectorAnnotation(method, redirect);
+            List<AnnotationNode> ats = Annotations.getValue(redirect, "at", true);
+            for (AnnotationNode at : ats) {
+                String value = Annotations.getValue(at);
+                if ("NEW".equals(value)) {
+                    wrapInjectorAnnotation(method, redirect);
+                    break;
+                }
             }
         }
     }
